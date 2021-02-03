602850ca0db314185b3b0d6bc71d48b7433d715e
busydog1G <stu112114@gmail.com>
2021-02-03 20:31:10 +0800

    ch7

--- ch7.py
+++ ch7.py
@@ -1,3 +1,21 @@
-x=12
-print(12)
-print(x)
+# while
+n=1
+sum=0
+while n<10:
+    print(n)
+    n+=1
+    sum+=n
+print(sum)
+
+# for
+for x in [1,3,5]:
+    print(x)
+
+for x in "ASDFFG":
+    print(x)
+
+for x in range(5): #range(5)=[0,1,2,3,4]
+    print(x)
+
+for x in range(5,10): #range(5,10)=[5,6,7,8,9]
+    print(x)
